4881c116c993a25b267dd40aecc6e47e2d2cf58e
AymericThiault <105485059+AymericThiault@users.noreply.github.com>
2023-03-26 18:34:47 +0200

    Create script.sh

---
+++ Project/update_dashboard.shcd/home/ubuntu/project  ./script.sh
@@ -0,0 +1,5 @@
+# I run this script using crontab, i add a new line in crontab -e which run this file every 5min that run script.sh (retrieving btc prices)
+# I tried to put directly script.sh but it didn't worked, even adding the executable (+x) rights
+
+cd /home/ubuntu/project
+./script.sh
